b2371dc0396a65fbe4610f987bc8a5943da44d37
kunalkeshan <kunalkeshan12@gmail.com>
2023-10-26 01:27:01 +0530

    fix: using generate meta data in page

--- app/(main)/products/[slug]/layout.tsx
+++ app/(main)/products/[slug]/layout.tsx
@@ -1,39 +1,3 @@
-import { client } from '@/sanity/lib/client';
-import { productCategoryBySlugQuery } from '@/sanity/lib/queries';
-import type { Metadata, ResolvingMetadata } from 'next';
-
-type Props = {
-	params: { slug: string };
-	searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export async function generateMetadata(
-	{ params, searchParams }: Props,
-	parent: ResolvingMetadata
-): Promise<Metadata> {
-	const category = await client.fetch<ProductCategory>(
-		productCategoryBySlugQuery,
-		params
-	);
-
-	return {
-		title: `${category.title} | Bellania`,
-		description: category.description,
-		openGraph: {
-			images: [category.image],
-			type: 'website',
-			title: `${category.title} | Bellania`,
-			description: category.description,
-		},
-		twitter: {
-			images: [category.image],
-			card: 'summary_large_image',
-			title: `${category.title} | Bellania`,
-			description: category.description,
-		},
-	};
-}
-
 export default function CategoryProductsLayout({
 	children,
 }: {
--- app/(main)/products/[slug]/page.tsx
+++ app/(main)/products/[slug]/page.tsx
@@ -1,6 +1,7 @@
 // ./nextjs-app/app/[slug]/page.tsx
 
 import {
+	productCategoryBySlugQuery,
 	productCategoryPathsQuery,
 	productsByCategoryQuery,
 } from '@/sanity/lib/queries';
@@ -10,6 +11,40 @@ import { client } from '@/sanity/lib/client';
 export async function generateStaticParams() {
 	const productCategories = await client.fetch(productCategoryPathsQuery);
 	return productCategories;
+}
+
+import type { Metadata, ResolvingMetadata } from 'next';
+
+type Props = {
+	params: { slug: string };
+	searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+	{ params, searchParams }: Props,
+	parent: ResolvingMetadata
+): Promise<Metadata> {
+	const category = await client.fetch<ProductCategory>(
+		productCategoryBySlugQuery,
+		params
+	);
+
+	return {
+		title: `${category.title} | Bellania`,
+		description: category.description,
+		openGraph: {
+			images: [category.image],
+			type: 'website',
+			title: `${category.title} | Bellania`,
+			description: category.description,
+		},
+		twitter: {
+			images: [category.image],
+			card: 'summary_large_image',
+			title: `${category.title} | Bellania`,
+			description: category.description,
+		},
+	};
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
