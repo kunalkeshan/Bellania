2d19138aecef76b074116edb7053683a353b4fb1
kunalkeshan <kunalkeshan12@gmail.com>
2023-10-26 16:41:51 +0530

    chore: added showcase to products page

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -1,5 +1,16 @@
-function Products() {
-	return <div>Home</div>;
+import { productCategoryQuery } from '@/sanity/lib/queries';
+import { sanityFetch } from '@/sanity/lib/sanityFetch';
+import Showcase from '@/components/home/Showcase';
+
+async function Products() {
+	const productCategory = await sanityFetch<ProductCategory[]>({
+		query: productCategoryQuery,
+	});
+	return (
+		<div>
+			<Showcase categories={productCategory} />
+		</div>
+	);
 }
 
 export default Products;
